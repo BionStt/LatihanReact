1a53549faa138710577668df5f2510119cadce0b
Sutanto Gasali <43228630+BionStt@users.noreply.github.com>
2023-03-26 11:42:21 +0700

    Add project files.

---
+++ LatihanReact/ClientApp/src/components/addition.js
@@ -0,0 +1,15 @@
+//import React, { Component } from 'react';
+import React from 'react';
+
+function Addition() {
+	return <p>The Sum is {2 * 5}</p>
+}
+
+//export class Addition extends Component {
+
+//    render() {
+//        return <p>The Sum is {2 * 5}</p>;
+//    }
+//}
+
+export default Addition;
